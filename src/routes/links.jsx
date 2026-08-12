f1c25e854f276af19967f25e8e34b90cfbbcb709
p10pro <15219491+jkaps9@users.noreply.github.com>
2026-08-12 09:18:14 -0400

    add links page content

--- src/routes/links.jsx
+++ src/routes/links.jsx
@@ -1,5 +1,25 @@
 const Links = () => {
-  return <h1>Customize your links</h1>;
+  return (
+    <>
+      <header>
+        <h1>Customize your links</h1>
+        <p>
+          Add/edit/remove links below and then share all your profiles with the
+          world!
+        </p>
+      </header>
+      <section>
+        <button type="button" className="btn btn--secondary">
+          + Add new link
+        </button>
+      </section>
+      <footer>
+        <button type="button" className="btn btn--primary">
+          Save
+        </button>
+      </footer>
+    </>
+  );
 };
 
 export default Links;
