cea7684a2391145614cee52fa0b80c1a4f8f3289
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-12 00:17:27 -0400

    feat: enhance header layout with navigation links and icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,37 @@ import { Link } from "react-router";
 import styles from "./App.module.css";
 import LogoLarge from "./assets/icons/logo-devlinks-large.svg";
 import LogoSmall from "./assets/icons/logo-devlinks-small.svg";
+import LinkIcon from "./assets/icons/icon-link.svg";
+import ProfileIcon from "./assets/icons/icon-profile-details-header.svg";
 
 function App() {
   return (
     <>
-      <header>
-        <img
-          src={LogoLarge}
-          alt="Dev Links Logo"
-          className={styles.logoLarge}
-        />
-        <img
-          src={LogoSmall}
-          alt="Dev Links Logo"
-          className={styles.logoSmall}
-        />
+      <header className={`row ${styles.header}`}>
+        <div>
+          <img
+            src={LogoLarge}
+            alt="Dev Links Logo"
+            className={styles.hideOnMobile}
+          />
+          <img
+            src={LogoSmall}
+            alt="Dev Links Logo"
+            className={styles.mobileOnly}
+          />
+        </div>
         <nav className={styles.navList}>
-          <Link to="profile">View Profile</Link>
-          <Link to="preview">Preview Links</Link>
+          <Link to="preview" className={`row ${styles.navLink}`}>
+            <img src={LinkIcon} alt="" aria-hidden="true" />
+            <span className={styles.hideOnMobile}>Links</span>
+          </Link>
+          <Link to="profile" className={`row ${styles.navLink}`}>
+            <img src={ProfileIcon} alt="" aria-hidden="true" />
+            <span className={styles.hideOnMobile}>View Profile</span>
+          </Link>
         </nav>
+
+        <button>Preview</button>
       </header>
       <main></main>
     </>
