0079542700b4a8e190119d20820d9ee3b58fba6b
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-11 23:59:58 -0400

    feat: add responsive logo images to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router";
 import styles from "./App.module.css";
+import LogoLarge from "./assets/icons/logo-devlinks-large.svg";
+import LogoSmall from "./assets/icons/logo-devlinks-small.svg";
 
 function App() {
   return (
     <>
       <header>
+        <img
+          src={LogoLarge}
+          alt="Dev Links Logo"
+          className={styles.logoLarge}
+        />
+        <img
+          src={LogoSmall}
+          alt="Dev Links Logo"
+          className={styles.logoSmall}
+        />
         <nav className={styles.navList}>
           <Link to="profile">View Profile</Link>
           <Link to="preview">Preview Links</Link>
