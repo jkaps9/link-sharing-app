13206d8bcb4d7ccce6fc4039febbe1367e2aae5c
p10pro <15219491+jkaps9@users.noreply.github.com>
2026-08-12 08:43:59 -0400

    add profile details form

--- src/routes/profile.jsx
+++ src/routes/profile.jsx
@@ -1,5 +1,56 @@
 const Profile = () => {
-  return <h1>Profile</h1>;
+  return (
+    <>
+      <header>
+        <h1>Profile Details</h1>
+        <p>Add your details to create a personal touch to your profile</p>
+      </header>
+      <section>
+        <form>
+          <section>
+            <label for="avatar">Profile picture</label>
+
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              accept="image/png, image/jpeg"
+            />
+
+            <p>Image must be below 1024x1024px. Use PNG or JPG format.</p>
+          </section>
+          <section>
+            <div class="input-group">
+              <label for="firstName">First name</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                placeholder="e.g. John"
+                autocomplete="given-name"
+                required
+              />
+            </div>
+            <div class="input-group">
+              <label for="lastName">Last Name:</label>
+              <input
+                name="lastName"
+                id="lastName"
+                type="text"
+                placeholder="e.g. Appleseed"
+                autocomplete="family-name"
+                required
+              />
+            </div>
+            <div class="input-group">
+              <label for="email">Email:</label>
+              <input name="email" id="email" type="email" autocomplete="off" />
+            </div>
+          </section>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Profile;
