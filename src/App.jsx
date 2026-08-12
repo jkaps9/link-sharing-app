b622c4dda50ebecbc6f9883667678d9794a67e3d
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-11 23:37:01 -0400

    feat: add routing and basic components for login, register, profile, and preview pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import { Link } from "react-router";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Link to="login">Login</Link>
+      <Link to="register">Sign Up</Link>
+      <Link to="profile">View Profile</Link>
+      <Link to="preview">Preview Links</Link>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
@@ -116,7 +121,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./routes/login.jsx";
+import Register from "./routes/register.jsx";
+import Profile from "./routes/profile.jsx";
+import Preview from "./routes/preview.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: `${import.meta.env.BASE_URL}`,
+    element: <App />,
+  },
+  {
+    path: `${import.meta.env.BASE_URL}login`,
+    element: <Login />,
+  },
+  {
+    path: `${import.meta.env.BASE_URL}register`,
+    element: <Register />,
+  },
+  {
+    path: `${import.meta.env.BASE_URL}profile`,
+    element: <Profile />,
+  },
+  {
+    path: `${import.meta.env.BASE_URL}preview`,
+    element: <Preview />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
