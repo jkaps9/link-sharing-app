85c55d85f4fb42e1b92c5306c3ec952a73900f46
p10pro <15219491+jkaps9@users.noreply.github.com>
2026-08-12 20:16:21 -0400

    style user links

--- src/components/UserLink.jsx
+++ src/components/UserLink.jsx
@@ -9,31 +9,40 @@ const UserLink = ({ userLink, onDelete }) => {
   return (
     <>
       <div>
-        <header>
-          <div>
-            <h2>Link #{userLink?.order}</h2>
-          </div>
-          <button type="button" className="btn" onClick={onDelete}>
-            Remove
-          </button>
-        </header>
-        <div className="input-group">
-          <label htmlFor="platform">Platform</label>
-          <select id="platform" name="platform">
-            {platforms.map((p) => (
-              <option
-                value={p.toLowerCase().replace(" ", "_")}
-                selected={userLink.platform === p}
+        <form>
+          <section>
+            <header className="row">
+              <div>
+                <h2>Link #{userLink?.order}</h2>
+              </div>
+              <button
+                type="button"
+                className="btn btn--text"
+                onClick={onDelete}
               >
-                {p}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="input-group">
-          <label htmlFor="url">Link</label>
-          <input type="text" name="url" id="url" value={userLink?.url} />
-        </div>
+                Remove
+              </button>
+            </header>
+
+            <div className="input-group">
+              <label htmlFor="platform">Platform</label>
+              <select id="platform" name="platform">
+                {platforms.map((p) => (
+                  <option
+                    value={p.toLowerCase().replace(" ", "_")}
+                    selected={userLink.platform === p}
+                  >
+                    {p}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="input-group">
+              <label htmlFor="url">Link</label>
+              <input type="text" name="url" id="url" value={userLink?.url} />
+            </div>
+          </section>
+        </form>
       </div>
     </>
   );
