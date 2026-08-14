e6476b999585a4ccf8240db562a76dbcee42b567
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-13 23:08:46 -0400

    refactor: streamline UserLink component structure and integrate form handling in Links component

--- src/components/UserLink.jsx
+++ src/components/UserLink.jsx
@@ -22,66 +22,58 @@ const UserLink = ({ userLink, onDelete }) => {
 
   return (
     <>
-      <div>
-        <form>
-          <section>
-            <header className="row">
-              <div>
-                <h2>Link #{formData.order}</h2>
-              </div>
-              <button
-                type="button"
-                className="btn btn--text"
-                onClick={onDelete}
-              >
-                Remove
-              </button>
-            </header>
+      <section>
+        <header className="row">
+          <div>
+            <h2>Link #{formData.order}</h2>
+          </div>
+          <button type="button" className="btn btn--text" onClick={onDelete}>
+            Remove
+          </button>
+        </header>
 
-            <div className="input-group">
-              <label htmlFor="platform">Platform</label>
-              <select
-                id="platform"
-                name="platform"
-                className={styles.customSelect}
-                onChange={handleChange}
-              >
-                <button>
-                  <selectedcontent></selectedcontent>
-                </button>
-                <div className={styles.selectList}>
-                  <option value="" selected={formData.platform === ""}></option>
-                  {platforms.map((p) => (
-                    <option
-                      key={p.id}
-                      value={p.id}
-                      selected={formData.platform === p.name}
-                    >
-                      <img
-                        src={p.iconPath}
-                        alt={`${p.name} icon`}
-                        width="16"
-                        height="16"
-                      />
-                      <span>{p.name}</span>
-                    </option>
-                  ))}
-                </div>
-              </select>
+        <div className="input-group">
+          <label htmlFor="platform">Platform</label>
+          <select
+            id="platform"
+            name="platform"
+            className={styles.customSelect}
+            onChange={handleChange}
+          >
+            <button>
+              <selectedcontent></selectedcontent>
+            </button>
+            <div className={styles.selectList}>
+              <option value="" selected={formData.platform === ""}></option>
+              {platforms.map((p) => (
+                <option
+                  key={p.id}
+                  value={p.id}
+                  selected={formData.platform === p.name}
+                >
+                  <img
+                    src={p.iconPath}
+                    alt={`${p.name} icon`}
+                    width="16"
+                    height="16"
+                  />
+                  <span>{p.name}</span>
+                </option>
+              ))}
             </div>
-            <div className="input-group">
-              <label htmlFor="url">Link</label>
-              <input
-                type="text"
-                name="url"
-                id="url"
-                value={formData.url}
-                onChange={handleChange}
-              />
-            </div>
-          </section>
-        </form>
-      </div>
+          </select>
+        </div>
+        <div className="input-group">
+          <label htmlFor="url">Link</label>
+          <input
+            type="text"
+            name="url"
+            id="url"
+            value={formData.url}
+            onChange={handleChange}
+          />
+        </div>
+      </section>
     </>
   );
 };
--- src/routes/links.jsx
+++ src/routes/links.jsx
@@ -1,28 +1,17 @@
-import { useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage.js";
 import UserLink from "../components/UserLink.jsx";
 
-const initialState = [
-  {
-    id: crypto.randomUUID(),
-    order: 1,
-    platform: "GitHub",
-    url: "https://www.github.com/jkaps9",
-  },
-  {
-    id: crypto.randomUUID(),
-    order: 2,
-    platform: "Frontend Mentor",
-    url: "https://www.frontendmentor.io/jkaps9",
-  },
-];
 const Links = () => {
-  const [userLinks, setUserLinks] = useState(initialState);
+  const [userLinks, setUserLinks] = useLocalStorage("userLinks", []);
 
   const addLink = (newLink) => {
     setUserLinks((prev) => {
-      const maxItem = prev.reduce((previous, current) =>
-        previous.order > current.order ? previous : current,
-      );
+      const maxItem =
+        prev.length === 0
+          ? { order: 0 }
+          : prev.reduce((previous, current) =>
+              previous.order > current.order ? previous : current,
+            );
       newLink.order = maxItem.order + 1;
       return [...prev, newLink];
     });
@@ -40,8 +29,6 @@ const Links = () => {
           Add/edit/remove links below and then share all your profiles with the
           world!
         </p>
-      </header>
-      <section>
         <button
           type="button"
           className="btn btn--secondary"
@@ -56,22 +43,24 @@ const Links = () => {
         >
           + Add new link
         </button>
-        <ul>
+      </header>
+      <section>
+        <form>
           {userLinks.map((link) => (
-            <li key={link.id}>
-              <UserLink
-                userLink={link}
-                onDelete={() => removeLink(link.id)}
-              ></UserLink>
-            </li>
+            <UserLink
+              key={link.id}
+              userLink={link}
+              onDelete={() => removeLink(link.id)}
+            ></UserLink>
           ))}
-        </ul>
+
+          <footer>
+            <button type="submit" className="btn btn--primary">
+              Save
+            </button>
+          </footer>
+        </form>
       </section>
-      <footer>
-        <button type="button" className="btn btn--primary">
-          Save
-        </button>
-      </footer>
     </section>
   );
 };
