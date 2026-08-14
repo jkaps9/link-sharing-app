145b493b90ea7d2b43374d0c58ad0f10e91d4315
p10pro <15219491+jkaps9@users.noreply.github.com>
2026-08-14 15:59:17 -0400

    add back button to preview page

--- src/routes/preview.jsx
+++ src/routes/preview.jsx
@@ -1,5 +1,19 @@
+import { NavLink } from "react-router";
 const Preview = () => {
-  return <h1>Preview</h1>;
+  return (
+    <>
+      <header>
+        <nav>
+          <button type="button" classNamr="btn btn--primary">
+            <NavLink to={`${import.meta.env.BASE_URL}links`}>
+              <span>Back to Editor</span>
+            </NavLink>
+          </button>
+        </nav>
+      </header>
+      <h1>Preview</h1>
+    </>
+  );
 };
 
 export default Preview;
