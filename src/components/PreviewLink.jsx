cb167255ef6bd74593b680834b2f4774b1c01d19
p10pro <15219491+jkaps9@users.noreply.github.com>
2026-08-14 18:21:18 -0400

    style preview link

--- src/components/PreviewLink.jsx
+++ src/components/PreviewLink.jsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router";
 import { platforms } from "../data/platforms";
 import ArrowIcon from "../assets/icons/icon-arrow-right.svg";
-
+import styles from "../styles/PreviewLink.module.css";
 const PreviewLink = ({ link }) => {
   const platform = platforms.find((item) => item.id === link.platform);
   return (
-    <Link to={link.url}>
-      <img
-        src={platform.iconPath}
-        alt={`${platform.name} icon`}
-        width="16"
-        height="16"
-      />
-      <span>{platform.name}</span>
+    <Link to={link.url} className={styles.previewLink} data-id={link.platform}>
+      <div className={styles.previewPlatform}>
+        <img
+          src={platform.iconPath}
+          alt={`${platform.name} icon`}
+          width="16"
+          height="16"
+        />
+        <span>{platform.name}</span>
+      </div>
       <img src={ArrowIcon} alt="" aria-hidden="true" width="12" height="12" />
     </Link>
   );
