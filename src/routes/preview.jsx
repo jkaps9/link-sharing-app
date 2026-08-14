362dc02a37d09eb5ce253c7e840494c0cd4e29ee
p10pro <15219491+jkaps9@users.noreply.github.com>
2026-08-14 17:24:08 -0400

    add profile data to preview page

--- src/routes/preview.jsx
+++ src/routes/preview.jsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router";
+import { useLocalStorage } from "../hooks/useLocalStorage.js";
+
 const Preview = () => {
+  const [userLinks, setUserLinks] = useLocalStorage("userLinks", []);
+  const [profileData, setProfileData] = useLocalStorage("profileData", {});
+
   return (
     <>
-      <header className="row">
+      <header className="row card">
         <button type="button" className="btn btn--secondary">
           <Link to={`${import.meta.env.BASE_URL}links`}>
             <span>Back to Editor</span>
@@ -12,7 +17,21 @@ const Preview = () => {
           <span>Share Link</span>
         </button>
       </header>
-      <h1>Preview</h1>
+      <main>
+        <section>
+          <div class="card">
+            <img
+              src={`${profileData.avatar}`}
+              alt="user avatar"
+              className="avatar"
+            />
+            <p>
+              {profileData.firstName} {profileData.lastName}
+            </p>
+            <p>{profileData.email}</p>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
