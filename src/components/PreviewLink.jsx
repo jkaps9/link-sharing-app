fa1bdd30fea03beef3a6e79ed3d4475c15d7a5f2
p10pro <15219491+jkaps9@users.noreply.github.com>
2026-08-14 21:59:58 -0400

    add svgr

--- src/components/PreviewLink.jsx
+++ src/components/PreviewLink.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router";
 import { platforms } from "../data/platforms";
-import ArrowIcon from "../assets/icons/icon-arrow-right.svg";
+import ArrowIcon from "../assets/icons/icon-arrow-right.svg?react";
 import styles from "../styles/PreviewLink.module.css";
 const PreviewLink = ({ link }) => {
   const platform = platforms.find((item) => item.id === link.platform);
@@ -30,7 +30,7 @@ const PreviewLink = ({ link }) => {
         />
         <span>{platform.name}</span>
       </div>
-      <img src={ArrowIcon} alt="" aria-hidden="true" width="12" height="12" />
+      <ArrowIcon aria-hidden="true" width="12" height="12" />
     </Link>
   );
 };
