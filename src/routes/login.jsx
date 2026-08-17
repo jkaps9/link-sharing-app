d5ec66e943268be1c82aab6b82c0ff06f77a32e1
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-16 23:48:53 -0400

    feat: update Login component styling and structure with new form styles

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,62 +1,72 @@
 import { Link } from "react-router";
+import formStyles from "../styles/Forms.module.css";
 
 const Login = () => {
   return (
     <>
-      <form className="card">
-        <header>
+      <form className={`${formStyles.form} ${formStyles.authForm}`}>
+        <header className={formStyles.formHeader}>
           <h1>Login</h1>
           <p>Add your details below to get back into the app</p>
         </header>
 
-        <div className="input-group">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            placeholder="e.g. alex@email.com"
-          />
-          <p id="email-error" className="error-message">
-            Can't be empty
-          </p>
-        </div>
-        <div className="input-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Enter your password"
-          />
-          <p id="password-error" className="error-message">
-            Please check again
-          </p>
-        </div>
-
-        <div
-          style={{
-            alignItems: "center",
-            display: "flex",
-            flexDirection: "column",
-            gap: "1.5rem",
-            textAlign: "center",
-          }}
+        <section
+          className={`${formStyles.formSection} ${formStyles.authFormSection}`}
         >
-          <button
-            type="submit"
-            className="btn btn--primary"
-            style={{ width: "100%" }}
+          <div
+            className={`${formStyles.formInputGroup} ${formStyles.formInputGroupColumn}`}
           >
-            Login
-          </button>
-          <div className="footer__content">
-            <p>Don't have an account? </p>
-            <Link to="../signup" className="accent-text">
-              Create account
-            </Link>
+            <label htmlFor="email">Email address</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="e.g. alex@email.com"
+            />
+            <p id="email-error" className={formStyles.formErrorMessage}>
+              Can't be empty
+            </p>
           </div>
-        </div>
+          <div
+            className={`${formStyles.formInputGroup} ${formStyles.formInputGroupColumn}`}
+          >
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+            <p id="password-error" className={formStyles.formErrorMessage}>
+              Please check again
+            </p>
+          </div>
+
+          <footer
+            className={`${formStyles.formFooter} ${formStyles.authFormFooter}`}
+            style={{
+              alignItems: "center",
+              display: "flex",
+              flexDirection: "column",
+              gap: "1.5rem",
+              textAlign: "center",
+            }}
+          >
+            <button
+              type="submit"
+              className="btn btn--primary"
+              style={{ width: "100%" }}
+            >
+              Login
+            </button>
+            <div className="footer__content">
+              <p>Don't have an account? </p>
+              <Link to="../signup" className="accent-text">
+                Create account
+              </Link>
+            </div>
+          </footer>
+        </section>
       </form>
     </>
   );
