1d3827c20ae04269205b2cb9943c89f8cc369203
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-16 23:57:00 -0400

    feat: enhance Register component with new form styles and structure

--- src/routes/register.jsx
+++ src/routes/register.jsx
@@ -1,56 +1,67 @@
 import { Link } from "react-router";
+import formStyles from "../styles/Forms.module.css";
 
 const Register = () => {
   return (
     <>
-      <form className="card">
-        <header>
-          <h1>Login</h1>
-          <p>Add your details below to get back into the app</p>
+      <form className={`${formStyles.form} ${formStyles.authForm}`}>
+        <header className={formStyles.formHeader}>
+          <h1>Create account</h1>
+          <p>Let’s get you started sharing your links!</p>
         </header>
 
-        <div className="input-group">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            placeholder="e.g. alex@email.com"
-          />
-          <p id="email-error" className="error-message">
-            Can't be empty
-          </p>
-        </div>
-        <div className="input-group">
-          <label htmlFor="password">Create password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="At least 8 characters"
-          />
-          <p id="password-error" className="error-message">
-            Please check again
-          </p>
-        </div>
-        <div className="input-group">
-          <label htmlFor="confirmPassword">Confirm password</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            placeholder="At least 8 characters"
-          />
-          <p id="confirm-password-error" className="error-message">
-            Please check again
-          </p>
-        </div>
+        <section
+          className={`${formStyles.formSection} ${formStyles.authFormSection}`}
+        >
+          <div
+            className={`${formStyles.formInputGroup} ${formStyles.formInputGroupColumn}`}
+          >
+            <label htmlFor="email">Email address</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="e.g. alex@email.com"
+            />
+            <p id="email-error" className={formStyles.formErrorMessage}>
+              Can't be empty
+            </p>
+          </div>
+          <div
+            className={`${formStyles.formInputGroup} ${formStyles.formInputGroupColumn}`}
+          >
+            <label htmlFor="password">Create password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="At least 8 characters"
+            />
+            <p id="password-error" className={formStyles.formErrorMessage}>
+              Please check again
+            </p>
+          </div>
+          <div
+            className={`${formStyles.formInputGroup} ${formStyles.formInputGroupColumn}`}
+          >
+            <label htmlFor="confirmPassword">Confirm password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="At least 8 characters"
+            />
+            <p>Password must contain at least 8 characters</p>
+          </div>
+        </section>
 
-        <footer>
+        <footer
+          className={`${formStyles.formFooter} ${formStyles.authFormFooter}`}
+        >
           <button type="submit" className="btn btn--primary">
             Create new account
           </button>
-          <div className="footer__content">
+          <div>
             <p>Already have an account?</p>
             <Link to="../login" className="accent-text">
               Login
