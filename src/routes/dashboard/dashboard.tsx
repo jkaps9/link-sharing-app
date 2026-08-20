b1b37b20a0eaef74e441b5a8e7cae2e0fc713c13
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-20 16:14:36 -0400

    feat: update Dashboard component to use toast for error handling and improve icon imports

--- src/routes/dashboard/dashboard.tsx
+++ src/routes/dashboard/dashboard.tsx
@@ -1,17 +1,18 @@
 import { NavLink, Outlet, useOutletContext } from "react-router";
 import { supabase } from "../../lib/supabaseClient.js";
+import { toast } from "react-hot-toast";
 
 import styles from "../../styles/App.module.css";
 import LogoLarge from "../../assets/icons/logo-devlinks-large.svg?react";
 import LogoSmall from "../../assets/icons/logo-devlinks-small.svg?react";
 import LinkIcon from "../../assets/icons/icon-link.svg?react";
 import ProfileIcon from "../../assets/icons/icon-profile-details-header.svg?react";
-import PreviewIcon from "../../assets/icons/icon-preview-header.svg";
+import PreviewIcon from "../../assets/icons/icon-preview-header.svg?react";
 
 function Dashboard() {
   const signOut = async () => {
     const { error } = await supabase.auth.signOut();
-    if (error) alert(error.message);
+    if (error) toast.error(error.message);
   };
 
   return (
@@ -40,9 +41,13 @@ function Dashboard() {
             <button className="btn btn--text" onClick={signOut}>
               Sign out
             </button>
-            <NavLink to="../preview" className="btn btn--secondary">
+            <NavLink
+              to="../preview"
+              className="btn btn--secondary"
+              aria-label="Preview"
+            >
               <span className={styles.hideOnMobile}>Preview</span>
-              <img src={PreviewIcon} alt="" className={styles.mobileOnly} />
+              <PreviewIcon className={styles.mobileOnly} aria-hidden="true" />
             </NavLink>
           </div>
         </header>
