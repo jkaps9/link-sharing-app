1bc2cf79c8a4d9de577289e4729c87a9cf68255a
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-20 16:40:48 -0400

    feat: remove signOut function from supabaseClient module

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -5,10 +5,4 @@ const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 const supabase = createClient(supabaseUrl, supabasePublishableKey);
 
-async function signOut() {
-  const { error } = await supabase.auth.signOut();
-
-  return error;
-}
-
-export { supabase, signOut };
+export { supabase };
